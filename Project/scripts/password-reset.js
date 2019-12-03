cab900a9a700fa1c8f05e8a966a9da1eb3c545f4
Joe O'Regan <joeaoregan@yahoo.ie>
2019-12-03 22:15:44 +0000

    Login page, reset password, register link

---
+++ Project/scripts/password-reset.js
@@ -0,0 +1,10 @@
+/*
+password-reset.js
+J O'Regan
+03/12/2019
+
+Handle resetting passwords
+*/
+function resetPassword(){
+    document.getElementById("resetPasswordID").innerHTML = "Check your email to reset password";
+}
