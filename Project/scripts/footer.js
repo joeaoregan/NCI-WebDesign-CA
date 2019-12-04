3f729e21ecea1ca0c9eab5e82601210932bef370
Joe O'Regan <joeaoregan@yahoo.ie>
2019-12-04 15:46:28 +0000

    Footer link titles and GitHub icon

--- Project/scripts/footer.js
+++ Project/scripts/footer.js
@@ -12,23 +12,24 @@ function createFooter(className) {
         <footer class="`+className+`">
             <div>
                 <div style="float: left; width: 33%;">
-                    <a href="index.html">Home</a> |
-                    <a href="antibody.html">Antibody</a> |
-                    <a href="flappybird.html">Flappy Bird</a> |
-                    <a href="spaceinvaders.html">Space Invaders</a> |
-                    <a href="register.html">Register</a> |
-                    <a href="contact.html">Contact Us</a> |
-                    <a href="about.html">About</a> |
-                    <a href="login.html">Login</a>
+                    <a href="index.html" title="Return To Homepage">Home</a> |
+                    <a href="antibody.html" title="Play Antibody">Antibody</a> |
+                    <a href="flappybird.html" title="Play Flappy Bird">Flappy Bird</a> |
+                    <a href="spaceinvaders.html" title="Play Space Invaders">Space Invaders</a> |
+                    <a href="register.html" title="Register With Us">Register</a> |
+                    <a href="contact.html" title="Contact Us">Contact Us</a> |
+                    <a href="about.html" title="About J.I.M. Games">About</a> |
+                    <a href="login.html" title="Login And Reset User Details">Login</a>
                 </div>
 
 
                 <div style="float: right; width: 33%;">
                     <a href="https://www.facebook.com/JIM-107782680690362/" class="fa fa-facebook" target="_blank"
                         title="Follow Us On Facebook"></a>
-                    <a href="https://twitter.com/JIMGames6" class="fa fa-twitter" target="_blank"></a>
-                    <a href="https://www.linkedin.com/" class="fa fa-linkedin" target="_blank"></a>
-                    <a href="https://youtu.be/HQiAlmhXLqA" class="fa fa-youtube" target="_blank"></a>
+                    <a href="https://twitter.com/JIMGames6" class="fa fa-twitter" target="_blank" title="Follow Us On Twitter"></a>
+                    <a href="https://www.linkedin.com/" class="fa fa-linkedin" target="_blank" title="Follow Us On LinkedIn"></a>
+                    <a href="https://youtu.be/HQiAlmhXLqA" class="fa fa-youtube" target="_blank" title="View Our YouTube Channel"></a>
+                    <a href="https://github.com/joeaoregan/NCI-WebDesign-CA" class="fa fa-github target="_blank" title="View The GitHub Repository"></a>
                 </div>
             </div>
 
