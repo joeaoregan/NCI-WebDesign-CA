998bc7202af3ef414a865b3354b68d19dfa7693e
Joe O'Regan <joeaoregan@yahoo.ie>
2019-12-08 16:07:04 +0000

    antibody controller

--- scripts/controller.js
+++ scripts/controller.js
@@ -7,7 +7,7 @@ Modifies the viewport so the screen doesn't scale when double tapping buttons/sc
 //Only show the controller for mobile device
 if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
 	var controller = document.getElementById("controller-section");//Get the div containing the controller
-	//controller.style.display = "none";// Hide controller if not a mobile device
+	controller.style.display = "none";// Hide controller if not a mobile device
 	console.log('not mobile');
 } else {
 	//var viewport = document.querySelector("meta[name=viewport]");	
