7f1a26eeb3d7dd89d5bb18cbc8ce3018b1c6af85
Joe O'Regan <joeaoregan@yahoo.ie>
2019-12-08 15:58:15 +0000

    validator antibody viewport

--- game-antibody/controller.js
+++ game-antibody/controller.js
@@ -3,4 +3,8 @@ if(!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
 	var controller = document.getElementById("controller-section");//Get the div containing the controller
 	controller.style.display="none";// Hide controller if not a mobile device
 	console.log('not mobile');
+} else {
+	//var viewport = document.querySelector("meta[name=viewport]");	
+	// Moved from antibody.html as giving warning in validator
+	document.getElementById("viewport-meta").setAttribute("content", "user-scalable=no, initial-scale=0.275, maximum-scale=0.275, minimum-scale=0.275");
 }
